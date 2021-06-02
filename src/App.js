9d3bb7e85db0959199611e4181280a970bbb6b57
Pundir <kpundir@deloitte.com>
2021-06-02 13:23:22 +0530

    adding Header to our app
    Signed-off-by: Pundir <kpundir@deloitte.com>

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import HomePage from "./pages/Homepage/homepage.component";
 import { Route, Switch } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
