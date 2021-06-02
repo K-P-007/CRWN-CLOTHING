8d1b75b7f4cfee333dc35d135f7b64fd42024fd5
Pundir <kpundir@deloitte.com>
2021-06-02 19:28:17 +0530

    finished integration of firebase and google sign in

---
+++ src/pages/SignIn-SignUp/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignIn from "../../components/SignIn";
+
+const SignInAndSignUp = () => {
+  return (
+    <div className="sign_in_and_sign_up">
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInAndSignUp;
