aa77d468e5787e1bedfebecd7c7935d79928bf89
Pundir <kpundir@deloitte.com>
2021-06-04 15:10:24 +0530

    integrated cart dropdown with cart items
    Signed-off-by: Pundir <kpundir@deloitte.com>

--- src/components/CartDropdown/index.jsx
+++ src/components/CartDropdown/index.jsx
@@ -1,16 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
+import CartItem from "../CartItem";
 
 import CustomButton from "../CustomButton";
 
 import "./index.scss";
 
-const CartDropdown = () => {
+const CartDropdown = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
 };
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => {
+  return {
+    cartItems,
+  };
+};
+
+export default connect(mapStateToProps)(CartDropdown);
