75ea7897eae8f23e197442116280cdd2b470e79a
Pundir <kpundir@deloitte.com>
2021-06-08 12:10:45 +0530

    Completed version 1 of the app
    Signed-off-by: Pundir <kpundir@deloitte.com>

--- src/pages/Checkout/index.jsx
+++ src/pages/Checkout/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import CheckoutItem from "../../components/CheckoutItem";
+import StripeCheckoutButton from "../../components/StripeButton";
 import {
   selectCartItems,
   selectCartTotal,
@@ -33,6 +34,12 @@ const CheckoutPage = ({ cartItems, cartTotal }) => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <div className="total">TOTAL: ${cartTotal}</div>
+      <div className="test-warning">
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+      </div>
+      <StripeCheckoutButton price={cartTotal} />
     </div>
   );
 };
